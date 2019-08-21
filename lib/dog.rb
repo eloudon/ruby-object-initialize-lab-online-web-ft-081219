e0619d26316d1a801fddcd132c6d29e31a178190
Eric Loudon <loudon.eric@gmail.com>
2019-08-21 07:14:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,20 @@
 class Dog
-  def initialize(breed)
-    @breed = "Mutt"
+  def initialize (dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
 
-  def breed=(breed)
-    @breed = "Mutt"
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
 
-  def breed
-    @breed
-  end
-  def name=(name)
-    @name = name
-  end
   def name
     @name
   end
+  def breed
+    @breed
+  end
 end
 
-Dog.new = fido
-fido.name = "Fido"
-fido.breed
+  fido = Dog.new("Fido"
